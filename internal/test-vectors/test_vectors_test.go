aa7024851ebeb2241a49ae5acaa51937cf63ffc9
Ben Toews <benjamin.toews@gmail.com>
2023-09-22 08:55:12 -0600

    package for generating test vectors

---
+++ internal/test-vectors/test_vectors_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/alecthomas/assert/v2"
+	macaroon "github.com/superfly/macaroon"
+)
+
+func TestCaveatSerialization(t *testing.T) {
+
+	b, err := json.Marshal(caveats)
+	assert.NoError(t, err)
+
+	cs2 := macaroon.NewCaveatSet()
+	err = json.Unmarshal(b, cs2)
+	assert.NoError(t, err)
+	assert.Equal(t, caveats, cs2)
+
+	b, err = caveats.MarshalMsgpack()
+	assert.NoError(t, err)
+	cs2, err = macaroon.DecodeCaveats(b)
+	assert.NoError(t, err)
+	assert.Equal(t, caveats, cs2)
+}
